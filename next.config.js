fc47012229a6edcb7ab62145a0f536dcbd21d55f
Andres Felipe Saumet <andressaumet@gmail.com>
2021-08-24 08:11:35 -0500

    cambio nuevamente en la URL

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: false,
   env: {
     backendURL: 'https://node-send-react-server.herokuapp.com/',
-    frontendURL: 'https://keen-heyrovsky-350183.netlify.app/'
+    frontendURL: 'https://keen-heyrovsky-350183.netlify.app'
   }
 }
